0e0e52b2911c0073df06f8c8cfaa7321d03c8c0e
nikhilrana13 <nikhilrajput060@gmail.com>
2026-07-17 15:06:12 +0530

    Initialize backend server with Express and MongoDB configuration

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser" 
+import dotenv from "dotenv"
+import { configure } from "./config/db.js"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+// routes 
+
+
+
+// connect to db
+configure()
+
+
+
+// server running
+app.listen(PORT,()=>{
+    console.log(`Server is running in ${PORT}`)
+})
