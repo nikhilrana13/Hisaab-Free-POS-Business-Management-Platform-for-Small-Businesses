6bc75e7747987c713490d01da7b2472b37d80091
nikhilrana13 <nikhilrajput060@gmail.com>
2026-08-09 14:52:09 +0530

    feat(api): add OrderApi reset to resetAllApiCaches function

--- frontend/src/utils/resetApiCache.js
+++ frontend/src/utils/resetApiCache.js
@@ -1,4 +1,5 @@
 import { DashboardApi } from "@/redux/api/DashboardApi";
+import { OrderApi } from "@/redux/api/OrderApi";
 import { ProductApi } from "@/redux/api/ProductApi";
 
 
@@ -6,4 +7,5 @@ import { ProductApi } from "@/redux/api/ProductApi";
 export const resetAllApiCaches = () => (dispatch) => {
   dispatch(DashboardApi.util.resetApiState());
   dispatch(ProductApi.util.resetApiState());
+  dispatch(OrderApi.util.resetApiState())
 };
